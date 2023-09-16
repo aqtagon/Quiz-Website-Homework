404abdb57911ecc5dc862afb58536c914b0ca391
aqtagon <ahmed.danawi@gmail.com>
2023-09-16 16:49:06 -0700

    Added remainder of questions

--- script.js
+++ script.js
@@ -20,8 +20,49 @@ var questions = [
             "head",
         ],
         correctAnswer: "img"
+    },
+    {
+        question: "Which HTML element do we put the JavaScript inside of?",
+        answers: [
+            "<js>"
+            "<javascript>"
+            "<script>"
+            "<banana>"
+        ],
+        correctAnswer: "<script>"
+    },
+    {
+        question: "Which CSS property is used to change the text color of an element?",
+        answers: [
+            "css-color"
+            "text-color"
+            "color"
+            "css-text"
+        ],
+        correctAnswer: "color"
+    },
+    {
+        question: "Select the correct answer for which sign is used for jQuery for a shortcut",
+        answers: [
+            "$"
+            "!"
+            "#"
+            "@"
+    
+        ],
+        correctAnswer: "$"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: [
+            "function-functionName{}"
+            "myFunction=function()"
+            "function.myFunction[]"
+            "function myFunction()"
+        ],
+        correctAnswer: "function myFunction()"
     }
-]
+];
 startBtn.addEventListener("click", function(){
     //Need to hide start button by adding hide to the class
     // Need to show questions by taking away hide in the class 
